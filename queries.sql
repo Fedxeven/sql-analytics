21b22e3aca753778ed82d829309e0ab16cbf8196
Надежда <138684404+Fedxeven@users.noreply.github.com>
2026-09-06 15:35:11 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -3,3 +3,12 @@ SELECT SUM(p.price * o.quantity) AS total_revenue
 FROM orders o
 JOIN products p
     ON o.product_id = p.product_id;
+-- 2. Revenue by product
+SELECT
+    p.product_name,
+    SUM(p.price * o.quantity) AS revenue
+FROM orders o
+JOIN products p
+    ON o.product_id = p.product_id
+GROUP BY p.product_name
+ORDER BY revenue DESC;
